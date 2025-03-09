a258c86870728c5689b4bc4d8e7878957db310c7
eve-mac <eve.charbonneau.1@umontreal.ca>
2025-03-09 19:22:21 +0400

    chore: Add MeshFileReal import to osim_reader.py

--- biobuddy/model_readers/osim_reader.py
+++ biobuddy/model_readers/osim_reader.py
@@ -13,6 +13,7 @@ from biobuddy.components.rotations import Rotations
 from biobuddy.components.translations import Translations
 from biobuddy.components.segment_coordinate_system_real import SegmentCoordinateSystemReal
 from biobuddy.components.marker_real import MarkerReal
+from biobuddy.components.mesh_file_real import MeshFileReal
 from biobuddy.mesh_modifications.vtp_parser import read_vtp_file, write_vtp_file
 from biobuddy.mesh_modifications.mesh_cleaner import transform_polygon_to_triangles
 
