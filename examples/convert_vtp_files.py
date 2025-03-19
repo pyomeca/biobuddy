0e11201949a87a54a8854f26d712891ae8d2fbb1
Charbie <eve.charbonneau.1@umontreal.ca>
2025-03-19 11:53:53 +0400

    fix: MeshParser example

--- examples/convert_vtp_files.py
+++ examples/convert_vtp_files.py
@@ -4,15 +4,17 @@ This example shows how to convert vtp files to triangles.
 
 from pathlib import Path
 
-from biobuddy import VtpParser
+from biobuddy import MeshParser, MeshFormat
 
 
 if __name__ == "__main__":
 
     # Paths
     current_path_file = Path(__file__).parent
-    geometry_path = f"{current_path_file}/models/Geometry"
+    geometry_path = f"{current_path_file}/../external/opensim-models/Geometry"
     geometry_cleaned_path = f"{current_path_file}/models/Geometry_cleaned"
 
     # Convert the files
-    VtpParser(geometry_path=geometry_path, geometry_cleaned_path=geometry_cleaned_path)
+    mesh = MeshParser(geometry_folder=geometry_path)
+    mesh.process_meshes()
+    mesh.write(geometry_cleaned_path, format=MeshFormat.VTP)
