3c98e0ec20807c8b45e52a399f295205519c0198
eve-mac <eve.charbonneau.1@umontreal.ca>
2025-05-29 15:11:15 +0400

    comment

--- tests/test_model_creation.py
+++ tests/test_model_creation.py
@@ -275,7 +275,7 @@ def write_markers_to_c3d(save_path: str, model):
     # Write the data
     c3d.write(save_path)
 
-
+# TODO: @pariterre could you determine if this tests is worth to keep ?
 """
 def test_model_creation_from_data():
 
