fixed scaling test

biobuddy/model_modifiers/scale_tool.py
@@ -679,11 +679,13 @@ class ScaleTool:
             visualize_optimal_static_pose,
             method,
         )
-        self.replace_markers_on_segments_local_scs(marker_positions, marker_names, q_static, model_to_use)
 
         if make_static_pose_the_models_zero:
             self.make_static_pose_the_zero(q_static)
             self.scaled_model.segments_rt_to_local()
+            self.replace_markers_on_segments_local_scs(marker_positions, marker_names, q=np.zeros((self.scaled_model.nb_q, )), model_to_use=self.scaled_model)
+        else:
+            self.replace_markers_on_segments_local_scs(marker_positions, marker_names, q_static, model_to_use)
 
     def modify_muscle_parameters(self):
         """

tests/test_scaling.py
@@ -163,8 +163,8 @@ def test_scaling_wholebody():
     q_zeros = np.zeros((42, marker_positions.shape[2]))
     q_random = np.random.rand(42) * 2 * np.pi
 
-    # For debugging
-    visualize_model_scaling_output(scaled_model, osim_model_scaled, q_zeros, marker_names, marker_positions)
+    # # For debugging
+    # visualize_model_scaling_output(scaled_model, osim_model_scaled, q_zeros, marker_names, marker_positions)
 
     # TODO: Find out why there is a discrepancy between the OpenSim and BioBuddy scaling factors of the to the third decimal.
     # Scaling factors from scaling_factors.osim  (TODO: add the scaling factors in the osim parser)