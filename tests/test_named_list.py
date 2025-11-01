1a47a4ba85ad69004d09f96c218120ec89ce441b
eve-mac <eve.charbonneau.1@umontreal.ca>
2025-11-01 18:00:34 +0100

    fixed namedlist test

--- tests/test_named_list.py
+++ tests/test_named_list.py
@@ -16,14 +16,6 @@ class PropertyNamedObject:
         return self._name
 
 
-class MethodNamedObject:
-    def __init__(self, name_value):
-        self._name_value = name_value
-
-    def name(self):
-        return self._name_value
-
-
 def test_init_empty():
     """Test initialization of an empty NamedList."""
     named_list = NamedList()
@@ -54,7 +46,7 @@ def test_append():
     named_list._append(SimpleNamedObject("item1"))
     assert len(named_list) == 1
     assert named_list.keys() == ["item1"]
-    
+
     # Append another item
     named_list._append(SimpleNamedObject("item2"))
     assert len(named_list) == 2
@@ -63,9 +55,10 @@ def test_append():
 
 def test_append_no_name_attribute():
     """Test appending an item without a name attribute raises an error."""
+
     class NoNameObject:
         pass
-    
+
     named_list = NamedList()
     with pytest.raises(AttributeError, match="must have a name attribute"):
         named_list._append(NoNameObject())
@@ -79,20 +72,12 @@ def test_append_with_property_name():
     assert named_list.keys() == ["prop_item"]
 
 
-def test_append_with_method_name():
-    """Test appending an item with a method name."""
-    named_list = NamedList()
-    named_list._append(MethodNamedObject("method_item"))
-    assert len(named_list) == 1
-    assert named_list.keys() == ["method_item"]
-
-
 def test_remove():
     """Test removing items from a NamedList."""
     named_list = NamedList()
     named_list._append(SimpleNamedObject("item1"))
     named_list._append(SimpleNamedObject("item2"))
-    
+
     removed = named_list._remove("item1")
     assert len(named_list) == 1
     assert named_list.keys() == ["item2"]
@@ -103,7 +88,7 @@ def test_remove_nonexistent():
     """Test removing a nonexistent item raises an error."""
     named_list = NamedList()
     named_list._append(SimpleNamedObject("item1"))
-    
+
     with pytest.raises(AttributeError, match="cannot be removed because it it not in the list"):
         named_list._remove("nonexistent")
 
@@ -115,7 +100,7 @@ def test_getitem_by_index():
     item2 = SimpleNamedObject("item2")
     named_list._append(item1)
     named_list._append(item2)
-    
+
     assert named_list[0] is item1
     assert named_list[1] is item2
 
@@ -127,7 +112,7 @@ def test_getitem_by_name():
     item2 = SimpleNamedObject("item2")
     named_list._append(item1)
     named_list._append(item2)
-    
+
     assert named_list["item1"] is item1
     assert named_list["item2"] is item2
 
@@ -136,7 +121,7 @@ def test_getitem_nonexistent_name():
     """Test getting a nonexistent item by name raises an error."""
     named_list = NamedList()
     named_list._append(SimpleNamedObject("item1"))
-    
+
     with pytest.raises(RuntimeError, match="does not exist in the current object"):
         _ = named_list["nonexistent"]
 
@@ -144,7 +129,7 @@ def test_getitem_nonexistent_name():
 def test_getitem_invalid_key_type():
     """Test getting an item with an invalid key type raises an error."""
     named_list = NamedList()
-    
+
     with pytest.raises(TypeError, match="key must be int or str"):
         _ = named_list[1.5]
 
@@ -155,7 +140,7 @@ def test_setitem_by_index():
     item1 = SimpleNamedObject("item1")
     item2 = SimpleNamedObject("item2")
     named_list._append(item1)
-    
+
     named_list[0] = item2
     assert named_list[0] is item2
     assert named_list.keys() == ["item2"]
@@ -167,7 +152,7 @@ def test_setitem_by_name_existing():
     item1 = SimpleNamedObject("item1")
     item2 = SimpleNamedObject("item2")
     named_list._append(item1)
-    
+
     named_list["item1"] = item2
     assert named_list[0] is item2
     assert named_list.keys() == ["item2"]
@@ -177,7 +162,7 @@ def test_setitem_by_name_new():
     """Test setting a new item by name."""
     named_list = NamedList()
     item1 = SimpleNamedObject("item1")
-    
+
     named_list["new_key"] = item1
     assert len(named_list) == 1
     assert named_list[0] is item1
@@ -187,7 +172,7 @@ def test_setitem_by_name_new():
 def test_setitem_invalid_key_type():
     """Test setting an item with an invalid key type raises an error."""
     named_list = NamedList()
-    
+
     with pytest.raises(TypeError, match="key must be int or str"):
         named_list[1.5] = SimpleNamedObject("item1")
 
@@ -203,33 +188,20 @@ def test_repr_with_items():
     named_list = NamedList()
     named_list._append(SimpleNamedObject("item1"))
     named_list._append(SimpleNamedObject("item2"))
-    
+
     repr_str = repr(named_list)
     assert "item1" in repr_str
     assert "item2" in repr_str
     assert repr_str.startswith("{\n")
     assert repr_str.endswith("\n}")
-    assert repr_str[:56] == '{\n item1 : <test_named_list.SimpleNamedObject object at '
-    assert repr_str[67:124] == '>,\n item2 : <test_named_list.SimpleNamedObject object at '
+    assert repr_str[:56] == "{\n item1 : <test_named_list.SimpleNamedObject object at "
+    assert repr_str[67:124] == ">,\n item2 : <test_named_list.SimpleNamedObject object at "
+
 
 def test_keys():
     """Test getting the keys of a NamedList."""
     named_list = NamedList()
     named_list._append(SimpleNamedObject("item1"))
     named_list._append(SimpleNamedObject("item2"))
-    
+
     assert named_list.keys() == ["item1", "item2"]
-
-
-def test_mixed_name_types():
-    """Test a NamedList with mixed name types (property, method, attribute)."""
-    named_list = NamedList()
-    named_list._append(SimpleNamedObject("attr_item"))
-    named_list._append(PropertyNamedObject("prop_item"))
-    named_list._append(MethodNamedObject("method_item"))
-    
-    assert len(named_list) == 3
-    assert named_list.keys() == ["attr_item", "prop_item", "method_item"]
-    assert named_list["attr_item"].name == "attr_item"
-    assert named_list["prop_item"].name == "prop_item"
-    assert named_list["method_item"].name() == "method_item"
