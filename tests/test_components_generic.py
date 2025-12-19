e0b2d0a84e6f4f47a5f74e176768658c7ae4bc53
eve-mac <eve.charbonneau.1@umontreal.ca>
2025-12-19 16:34:14 -0500

    started to test

--- tests/test_components_generic.py
+++ tests/test_components_generic.py
@@ -22,6 +22,7 @@ from biobuddy import (
     Translations,
     Rotations,
     RotoTransMatrix,
+    SegmentCoordinateSystemUtils,
 )
 from biobuddy.utils.named_list import NamedList
 from test_utils import MockC3dData, get_xml_str
@@ -1052,6 +1053,24 @@ def test_segment_coordinate_system_to_scs_local():
             ]
         ),
     )
+
+
+# ------- SegmentCoordinateStstemUtils ------- #
+def test_rigidify():
+    # Define a set of markers
+    markers = np.array([
+        [0.05, 0.12, -0.19],
+        [0.04, 0.01, 0.16],
+        [0.01, -0.02, 0.04],
+        [-0.05, 0.10, -0.09],
+        [0.15, 0.15, 0.03],
+        [-0.001, 0.07, 0.10],
+    ])
+    # Rigidify the markers
+    rigidified_markers = SegmentCoordinateSystemUtils.rigidify(data=markers, reference_data=)
+
+
+
 
 
 # ------- Mesh ------- #
