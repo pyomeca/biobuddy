620882e163da6104b8adae3e99602d48ce4638f0
Charbie <eve.charbonneau.1@umontreal.ca>
2026-04-07 09:16:44 -0400

    Added a test for bad str

--- tests/test_biomod_parser.py
+++ tests/test_biomod_parser.py
@@ -18,7 +18,7 @@ def test_pi_parsing():
     root_path = parent_path + "/examples/models"
     biomod_filepath = f"{root_path}/pendulum.bioMod"
 
-    # Convert osim to biomod
+    # Load the model
     model = BiomechanicalModelReal().from_biomod(filepath=biomod_filepath)
 
     # Check the pi values
@@ -27,3 +27,28 @@ def test_pi_parsing():
     npt.assert_almost_equal(q_min, np.array([-1, -2*np.pi]))
     npt.assert_almost_equal(q_max, np.array([5, 2*np.pi]))
 
+def test_problematic_parsing():
+    """
+    Read a simple biomod file that contains an unknown expressions
+    """
+
+    # Paths
+    parent_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    root_path = parent_path + "/examples/models"
+    biomod_filepath = f"{root_path}/pendulum.bioMod"
+
+    # Replace a line with a wrong expression
+    with open(biomod_filepath, "r") as f:
+        content = f.read()
+    content = content.replace("com  -0.0005 0.0688 -0.9542", "com  -0.0005 0.0688 bad_name")
+
+    bad_file_name = biomod_filepath.replace(".bioMod", "_bad.bioMod")
+    with open(bad_file_name, "w") as f:
+        f.write(content)
+
+    # Load the model
+    with pytest.raises(ValueError, match="could not convert string to float: 'bad_name'"):
+        model = BiomechanicalModelReal().from_biomod(filepath=bad_file_name)
+
+    # Delete the bad file created
+    os.remove(bad_file_name)
