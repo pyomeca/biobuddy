4c6b32cdb9fd340f0cd11ab5227b6de9b61d650d
mickaelbegon <mickaelbegon@gmail.com>
2026-06-09 14:43:04 -0400

    Add real Yeadon sample regression test

--- tests/test_yeadon_characteristics.py
+++ tests/test_yeadon_characteristics.py
@@ -13,6 +13,104 @@ from biobuddy import (
     YeadonTable,
 )
 
+MALE1_MEASUREMENTS_CM = {
+    "Ls1L": 4.9,
+    "Ls2L": 18.8,
+    "Ls3L": 33.4,
+    "Ls4L": 47.2,
+    "Ls5L": 50.2,
+    "Ls6L": 14.9,
+    "Ls7L": 19.0,
+    "Ls8L": 29.2,
+    "Ls0p": 86.9,
+    "Ls1p": 75.0,
+    "Ls2p": 73.9,
+    "Ls3p": 80.6,
+    "Ls5p": 37.5,
+    "Ls6p": 49.8,
+    "Ls7p": 57.0,
+    "Ls0w": 32.4,
+    "Ls1w": 27.4,
+    "Ls2w": 25.1,
+    "Ls3w": 28.0,
+    "Ls4w": 30.3,
+    "Ls4d": 15.1,
+    "La2L": 26.1,
+    "La3L": 33.0,
+    "La4L": 50.8,
+    "La5L": 3.0,
+    "La6L": 8.8,
+    "La7L": 17.7,
+    "La0p": 28.5,
+    "La1p": 24.2,
+    "La2p": 23.0,
+    "La3p": 23.5,
+    "La4p": 15.4,
+    "La5p": 21.0,
+    "La6p": 21.5,
+    "La7p": 10.7,
+    "La4w": 5.0,
+    "La5w": 7.3,
+    "La6w": 9.8,
+    "La7w": 4.9,
+    "Lb2L": 26.6,
+    "Lb3L": 32.4,
+    "Lb4L": 51.9,
+    "Lb5L": 1.5,
+    "Lb6L": 7.2,
+    "Lb7L": 16.6,
+    "Lb0p": 29.0,
+    "Lb1p": 24.2,
+    "Lb2p": 22.7,
+    "Lb3p": 23.4,
+    "Lb4p": 15.2,
+    "Lb5p": 20.9,
+    "Lb6p": 22.4,
+    "Lb7p": 11.0,
+    "Lb4w": 5.2,
+    "Lb5w": 7.2,
+    "Lb6w": 9.6,
+    "Lb7w": 5.0,
+    "Lj1L": 10.1,
+    "Lj3L": 42.6,
+    "Lj4L": 56.5,
+    "Lj5L": 81.6,
+    "Lj6L": 2.5,
+    "Lj8L": 14.9,
+    "Lj9L": 21.0,
+    "Lj1p": 52.0,
+    "Lj2p": 48.9,
+    "Lj3p": 35.1,
+    "Lj4p": 35.2,
+    "Lj5p": 25.5,
+    "Lj6p": 30.2,
+    "Lj7p": 23.3,
+    "Lj8p": 22.8,
+    "Lj9p": 18.5,
+    "Lj8w": 8.7,
+    "Lj9w": 8.2,
+    "Lj6d": 11.4,
+    "Lk1L": 9.7,
+    "Lk3L": 42.2,
+    "Lk4L": 55.8,
+    "Lk5L": 80.6,
+    "Lk6L": 2.5,
+    "Lk8L": 14.5,
+    "Lk9L": 20.8,
+    "Lk1p": 55.0,
+    "Lk2p": 50.0,
+    "Lk3p": 34.8,
+    "Lk4p": 34.9,
+    "Lk5p": 23.0,
+    "Lk6p": 31.0,
+    "Lk7p": 23.7,
+    "Lk8p": 22.2,
+    "Lk9p": 19.5,
+    "Lk8w": 8.6,
+    "Lk9w": 8.3,
+    "Lk6d": 11.1,
+}
+
 
 class FakeSegment:
     def __init__(self, index: int, name: str):
@@ -128,3 +226,17 @@ def test_yeadon_measurement_specs_cover_all_measurements():
         "Left leg",
         "Right leg",
     }
+
+
+def test_yeadon_table_matches_real_yeadon_sample():
+    pytest.importorskip("yeadon")
+    measurements = {name: value * 0.01 for name, value in MALE1_MEASUREMENTS_CM.items()}
+
+    table = YeadonTable(measurements)
+
+    npt.assert_almost_equal(table.mass, 58.200488588422544)
+    npt.assert_array_almost_equal(table.inertia, np.diag([9.63093850, 9.99497872, 5.45117742e-01]))
+    npt.assert_array_almost_equal(table.center_of_mass, np.array([[0.0], [0.0], [1.19967938e-02]]))
+    npt.assert_almost_equal(table[YeadonSegmentName.PELVIS].mass, table.human.P.mass)
+    npt.assert_array_almost_equal(table[YeadonSegmentName.PELVIS].center_of_mass[:3], table.human.P.rel_center_of_mass)
+    npt.assert_array_almost_equal(table[YeadonSegmentName.PELVIS].inertia[:3, :3], table.human.P.rel_inertia)
